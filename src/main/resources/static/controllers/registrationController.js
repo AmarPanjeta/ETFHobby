b05e61d17eda2cc60c254489d66032a4522fd57b
LejlaNukic <lejla.nukic94@gmail.com>
2016-11-01 13:15:09 +0100

    registration added

--- src/main/resources/static/controllers/registrationController.js
+++ src/main/resources/static/controllers/registrationController.js
@@ -1,3 +1,10 @@
-app.controller('registrationController',function(){
-  
+app.controller('registrationController',function($http,$scope,$location){
+ $scope.user={}
+ $scope.register=function(){	
+ 	if ($scope.passwordRepeat==$scope.user.password){
+ 		$http.post("http://localhost:8080/user/registration",$scope.user).then(function(response){
+ 			$location.path("/")
+ 		})
+ 	}
+ }
 })
