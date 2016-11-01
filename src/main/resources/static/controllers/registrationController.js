145f59c4a80143750c68347beaf3a8403d5a6e44
LejlaNukic <lnukic2@etf.unsa.ba>
2016-11-01 13:18:18 +0100

    comment added

--- src/main/resources/static/controllers/registrationController.js
+++ src/main/resources/static/controllers/registrationController.js
@@ -1,5 +1,6 @@
 app.controller('registrationController',function($http,$scope,$location){
  $scope.user={}
+ //izmjena
  $scope.register=function(){	
  	if ($scope.passwordRepeat==$scope.user.password){
  		$http.post("http://localhost:8080/user/registration",$scope.user).then(function(response){
