e08b0f3391ebd48fa0b49e91a34dd0802c1cd6a0
LejlaNukic <lnukic2@etf.unsa.ba>
2016-11-04 19:27:11 +0100

    checkRouting missing

--- src/main/resources/static/app.routes.js
+++ src/main/resources/static/app.routes.js
@@ -8,5 +8,32 @@ app.config(function($routeProvider){
   }).when('/registration',{
     templateUrl:"partials/registrationPage.html",
     controller:"registrationController"
-  })
-})
+  }).when("/hobbies",{
+    templateUrl: "partials/hobbiesPage.html",
+     /* resolve: {
+       factory: checkRouting
+                }
+                */
+    
+  }).otherwise("/")
+});
+
+/*
+var checkRouting= function (TokenService,$q, $rootScope, $location,$http) {
+   if ($rootScope.token !== null) {
+        return true;
+    } else {
+        var deferred = $q.defer();
+        TokenService.tokenExists() //obrisano
+            .success(function (response) {
+                deferred.resolve(true);
+            })
+            .error(function () {
+                deferred.reject();
+                $location.path("/");
+             });
+        return deferred.promise;
+    }
+    
+};
+*/
