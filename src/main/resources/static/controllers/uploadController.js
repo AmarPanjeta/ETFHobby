6a5d0b9e65dce1bfb5baa7b48e9543f7f35bb8cb
LejlaNukic <lnukic2@etf.unsa.ba>
2017-01-10 14:39:28 +0100

    design details added

--- src/main/resources/static/controllers/uploadController.js
+++ src/main/resources/static/controllers/uploadController.js
@@ -1,10 +1,12 @@
-function fileCtrl ($scope,$rootScope) {
+function fileCtrl ($scope,$rootScope,$location) {
     $scope.partialDownloadLink = 'http://localhost:8080/download?filename=';
     $scope.filename = '';
 
     $scope.username=$rootScope.userName;
     $scope.uploadFile = function() {
         $scope.processDropzone();
+        $location.path("/profile");
+
     };
 
     $scope.reset = function() {
